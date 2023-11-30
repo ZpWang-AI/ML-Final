12375672d4a652bc96cbe8a782c8bf877bc943f3
zpwang <zhipangwang@gmail.com>
2023-11-30 15:43:31 +0800

    modified:   .gitignore 	new file:   data/data.zip 	renamed:    src/data_processing_demo.py -> data/data_processing_demo.py 	deleted:    data/empty 	renamed:    "notes/\346\225\260\346\215\256\351\233\206\350\257\264\346\230\216.md" -> "data/notes/\346\225\260\346\215\256\351\233\206\350\257\264\346\230\216.md" 	modified:   src/model/LSTM.py

--- src/model/LSTM.py
+++ src/model/LSTM.py
@@ -1,1 +1,38 @@
 import torch
+import torch.nn as nn
+
+
+class LSTM(nn.Module):
+    def __init__(self, data_dim, hidden_size, num_layers, dropout=0., ) -> None:
+        self.lstm = nn.LSTM(
+            input_size=data_dim,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bias=True,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=False,
+        )
+        self.classifier = nn.Linear(
+            in_features=hidden_size,
+            out_features=data_dim,
+        )
+    
+    def forward(self, x, label):
+        """
+        x: [batch size, seq length, data dim]
+        h: [batch size, seq length, data dim]
+        ht: [num layers, batch size, hidden dim]
+        ct: [num layers, batch size, hidden dim]
+        m: [batch size, data dim]
+        """
+        h, (ht, ct) = self.lstm(x)
+        m = h[:,-1,]
+        # m = ht[-1,...]
+        logits = self.classifier(m)
+        
+        pass
+
+
+if __name__ == '__main__':
+    pass
