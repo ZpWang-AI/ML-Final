958f44581662faf5b9dea0dac2d23a0295c2986d
zpwang <zhipangwang@gmail.com>
2023-11-30 19:11:33 +0800

    modified:   src/metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,34 +1,35 @@
 import numpy as np
 
-from sklearn.metrics import f1_score, accuracy_score
+
+def cal_MSE(mat1, mat2):
+    return ((mat1-mat2)**2).sum() / len(mat1)
+
+def cal_MAE(mat1, mat2):
+    return (np.abs(mat1-mat2)).sum() / len(mat1)
 
 
 class ComputeMetrics:
-    def __init__(self, label_list:list) -> None:
-        self.label_list = label_list
-        self.metric_names = ['Macro-F1', 'Acc']+label_list
+    def __init__(self, feature_list:list) -> None:
+        self.feature_list = feature_list
+        self.metric_names = ['MSE', 'MAE']  # +feature_list
         
     def __call__(self, eval_pred):
         """
-        n = label categories
+        n = data dimension
         eval_pred: (predictions, labels)
         predictions: np.array [datasize, n]
         labels: np.array [datasize, n]
-        X[p][q]=True, sample p belongs to label q (False otherwise)
         """
         predictions, labels = eval_pred
-        predictions = np.argmax(predictions, axis=1)
-        predictions = np.eye(len(self.label_list))[predictions]
-        labels = (labels != 0).astype(int)
         
         res = {
             # 'Acc': accuracy_score(labels, predictions),
-            'Macro-F1': f1_score(labels, predictions, average='macro', zero_division=0),
-            'Acc': np.sum(predictions*labels)/len(predictions),
+            'MSE': cal_MSE(predictions, labels),
+            'MAE': cal_MAE(predictions, labels),
         }
         
-        for i, target_type in enumerate(self.label_list):
-            res[target_type] = f1_score(predictions[:,i], labels[:,i], zero_division=0)
+        # for i, target_type in enumerate(self.feature_list):
+        #     res[target_type] = cal_MSE(predictions[:])
         
         return res
     
