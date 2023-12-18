d42b255ec7adf671cac58ba5367f196ad5feae97
zpwang <zhipangwang@gmail.com>
2023-12-18 22:02:28 +0800

    modified:   src/model/Transformer.py

--- src/model/Transformer.py
+++ src/model/Transformer.py
@@ -54,17 +54,8 @@ class Transformer(nn.Module):
         tgt_emb = self.decoder_projection(tgt)
         src_emb += self.get_pos_emb(src_emb, self.encoder_pos_embedding)
         tgt_emb += self.get_pos_emb(tgt_emb, self.decoder_pos_embedding)
-        if self.training:
-            tgt_mask = self.transformer.generate_square_subsequent_mask(tgt.shape[1], tgt.device)
-        else:
-            tgt_mask = self.transformer.generate_square_subsequent_mask(tgt.shape[1], tgt.device)
-        #     tgt_mask = None
 
-        output = self.transformer(
-            src=src_emb, 
-            tgt=tgt_emb,
-            tgt_mask=tgt_mask,
-        )
+        output = self.transformer(src=src_emb, tgt=tgt_emb,)
         output = self.linear(output)
         return output
     
@@ -78,23 +69,11 @@ class Transformer(nn.Module):
         pred: [batch size, 96/336, data_dim]
         """
         inputs = inputs[..., :self.data_dim]
-        if self.training:
-            src = inputs[:, :95, ]
-            tgt = inputs[:, :, ]
-            y = inputs[:, 96:, ]
-            output = self.model_forward(src, tgt)
-            loss = self.criterion(output, y)
-            return {'pred':output, 'gt':y, 'loss':loss}
-        else:
-            src = inputs[:, :95, ]
-            tgt_start = inputs[:, 95:96, ]
-            y = inputs[:, 96:, ]
-            pred = self.model_forward(src, tgt_start)
-            while pred.shape[1] < y.shape[1]:
-                tgt = torch.concat([tgt_start, pred], dim=1)
-                output = self.model_forward(src, tgt)
-                pred = torch.concat([pred, output[:, -1:, ]], dim=1)
-                print(tgt.shape, output.shape, pred.shape)
-                # print((output!=pred).sum())
-            loss = self.criterion(pred, y)
-        return {'pred': pred, 'gt': y, 'loss': loss}
+        src = inputs[:, :96, ]
+        tgt = inputs[:, 48:96, ]
+        y = inputs[:, 96:, ]
+        tgt = torch.concat([tgt, torch.zeros_like(y)], dim=1)
+        output = self.model_forward(src, tgt)
+        pred = output[:, 48:, ]
+        loss = self.criterion(pred, y)
+        return {'pred':pred, 'gt':y, 'loss':loss}
